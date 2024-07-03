5aa3ccf1cb8ffaa19724418a27b1903a215c0509
nadil11 <nadilnidhal111@gmail.com>
2024-07-03 19:43:25 +0530

    last commit

--- client/src/components/sidebar/LogoutButton.jsx
+++ client/src/components/sidebar/LogoutButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BiLogOut } from "react-icons/bi";
-import useLogout from '../hooks/useLogout';
+import useLogout from '../../hooks/useLogout';
 
 const LogoutButton = () => {
 
